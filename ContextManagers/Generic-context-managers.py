ddd048e2236efd61732db7eb23ca80b23a8a8805
Joel Joseph Thomas <joeljoseph26@Joels-MacBook-Pro.local>
2024-01-19 21:50:07 -0500

    changes made

--- ContextManagers/Generic-context-managers.py
+++ ContextManagers/Generic-context-managers.py
@@ -1,5 +1,28 @@
 class ManagedFile:
-    def __init__(self,message):
-        self.message = message
+    def __init__(self, filename):
+        print('init', filename)
+        self.filename = filename
+
+    def __enter__(self):
+        print('enter')
+        self.file = open(self.filename, 'w')
+        return self.file
         
-        
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if self.file:
+            self.file.close()
+        if exc_type is not None:
+            print('Exception has been handled')
+        print('exit')
+        return True
+
+with ManagedFile('./ContextManagers/notes.txt') as f:
+    print('doing stuff...')
+    f.write('some todo...')
+print('continuing...')
+
+with ManagedFile('./ContextManagers/notes2.txt') as f:
+    print('doing stuff...')
+    f.write('some todo...')
+    f.do_something()
+print('continuing...')
---
+++ MySQL/mysql.py
@@ -0,0 +1,18 @@
+import mysql
+
+# my_db = mysql.connector.connect(
+#     host="http://localhost:8891/notebooks/Documents/Education"
+#     password="abc",
+#     port=80,
+#     user="username")
+
+# my_cursor = my_db.cursor()
+# my_cursor.execute("CREATE TABLE MYSQL_TABLE")
+# my_cursor.executemany("CREATE TABLE MYSQL_TABLE")
+# my_cursor.fetchone()
+# my_cursor.fetchall()
+# my_db.commit()
+
+# # e.g.: my_cursor.rowcount
+# rowcount
+# lastrowid
